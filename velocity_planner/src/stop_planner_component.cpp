f76d33feb4a71eb5c140f9fcaa0de0b3b49c85da
Masaya Kataoka <ms.kataoka@gmail.com>
2020-03-03 21:24:53 +0900

    rename to stop_planner

--- velocity_planner/src/stop_planner_component.cpp
+++ velocity_planner/src/stop_planner_component.cpp
@@ -1,9 +1,9 @@
-#include <velocity_planner/velocity_constraint_planner_component.h>
+#include <velocity_planner/stop_planner_component.h>
 
 namespace velocity_planner
 {
-    VelocityConstraintPlannerComponent::VelocityConstraintPlannerComponent(const rclcpp::NodeOptions & options)
-    : Node("velocity_constraint_planner", options)
+    StopPlannerComponent::StopPlannerComponent(const rclcpp::NodeOptions & options)
+    : Node("stop_planner", options), generator_(0.0), viz_(get_name())
     {
         hermite_path_pub_ = this->create_publisher<hermite_path_msgs::msg::HermitePathStamped>
             ("~/hermite_path", 1);
@@ -11,10 +11,10 @@ namespace velocity_planner
         declare_parameter("hermite_path_topic","/hermite_path_planner/hermite_path");
         get_parameter("hermite_path_topic",hermite_path_topic);
         hermite_path_sub_ = this->create_subscription<hermite_path_msgs::msg::HermitePathStamped>
-            (hermite_path_topic, 1, std::bind(&VelocityConstraintPlannerComponent::hermitePathCallback, this, std::placeholders::_1));
+            (hermite_path_topic, 1, std::bind(&StopPlannerComponent::hermitePathCallback, this, std::placeholders::_1));
     }
 
-    void VelocityConstraintPlannerComponent::hermitePathCallback(const hermite_path_msgs::msg::HermitePathStamped::SharedPtr data)
+    void StopPlannerComponent::hermitePathCallback(const hermite_path_msgs::msg::HermitePathStamped::SharedPtr data)
     {
         path_ = *data;
         hermite_path_pub_->publish(path_.get());
--- velocity_planner/src/stop_planner_node.cpp
+++ velocity_planner/src/stop_planner_node.cpp
@@ -1,11 +1,11 @@
-#include <velocity_planner/velocity_constraint_planner_component.h>
+#include <velocity_planner/stop_planner_component.h>
 #include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::NodeOptions options;
-    auto component = std::make_shared<velocity_planner::VelocityConstraintPlannerComponent>(options);
+    auto component = std::make_shared<velocity_planner::StopPlannerComponent>(options);
     rclcpp::spin(component);
     rclcpp::shutdown();
     return 0;
