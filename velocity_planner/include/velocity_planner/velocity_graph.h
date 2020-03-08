0a480aa48ff6e0daa9a194514b2743c768ca1ed9
Masaya Kataoka <ms.kataoka@gmail.com>
2020-03-08 13:05:15 +0900

    add make Node and Edge function

--- velocity_planner/include/velocity_planner/velocity_graph.h
+++ velocity_planner/include/velocity_planner/velocity_graph.h
@@ -1,16 +1,51 @@
 #ifndef VELOCITY_PLANNER_VELOCITY_GRAPH_H_INCLUDED
 #define VELOCITY_PLANNER_VELOCITY_GRAPH_H_INCLUDED
 
+#include <hermite_path_planner/hermite_path_generator.h>
+#include <hermite_path_msgs/msg/hermite_path_stamped.hpp>
+#include <hermite_path_msgs/msg/reference_velocity.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <map>
+
 namespace velocity_planner
 {
     struct Node
     {
-        double linear_velocity;
+        hermite_path_msgs::msg::ReferenceVelocity vel;
     };
 
     struct Edge
     {
+        Node before_node;
+        Node after_node;
         double linear_accerelation;
+    };
+
+    struct Graph
+    {
+
+    };
+
+    typedef boost::adjacency_list<boost::listS,boost::vecS,
+        boost::bidirectionalS,Node,Edge,Graph> VelocityGraphData;
+
+    class VelocityGraph
+    {
+    public:
+        VelocityGraph(hermite_path_msgs::msg::HermitePathStamped data,
+            double velocity_resoluation,
+            double maximum_accerelation,
+            double minimum_accerelation);
+    private:
+        std::vector<Node> makeNodes(hermite_path_msgs::msg::ReferenceVelocity vel);
+        boost::optional<std::vector<Edge> > makeEdges(std::map<double,std::vector<Node> > nodes);
+        VelocityGraphData data_;
+        double velocity_resoluation_;
+        double maximum_accerelation_;
+        double minimum_accerelation_;
+        double path_length_;
+        hermite_path_planner::HermitePathGenerator generator_;
     };
 }
 
--- velocity_planner/src/velocity_graph.cpp
+++ velocity_planner/src/velocity_graph.cpp
@@ -1,1 +1,78 @@
 #include <velocity_planner/velocity_graph.h>
+
+namespace velocity_planner
+{
+    VelocityGraph::VelocityGraph(hermite_path_msgs::msg::HermitePathStamped data,
+        double velocity_resoluation,
+        double maximum_accerelation,
+        double minimum_accerelation) : generator_(0.0)
+    {
+        minimum_accerelation_ = minimum_accerelation;
+        maximum_accerelation_ = maximum_accerelation;
+        velocity_resoluation_ = velocity_resoluation;
+        path_length_ = generator_.getLength(data.path,200);
+        std::map<double,std::vector<Node> > nodes;
+        for(auto itr=data.reference_velocity.begin(); itr!=data.reference_velocity.end(); itr++)
+        {
+            nodes[itr->t] = makeNodes(*itr);
+        }
+    }
+
+    boost::optional<std::vector<Edge> > VelocityGraph::makeEdges(std::map<double,std::vector<Node> > nodes)
+    {
+        std::vector<Edge> edges;
+        std::vector<double> t_values;
+        for(auto it = nodes.begin(); it != nodes.end(); ++it)
+        {
+            t_values.push_back(it->first);
+        }
+        std::sort(t_values.begin(), t_values.end(), [](const auto &a, const auto &b){return a < b;});
+        for(int i=0 ; i<((int)t_values.size()-1); i++)
+        {
+            bool connection_finded = false;
+            std::vector<Node> before_nodes = nodes[i];
+            std::vector<Node> after_nodes = nodes[i+1];
+            double l = (t_values[i+1]-t_values[i])*path_length_;
+            for(auto before_itr = before_nodes.begin(); before_itr != before_nodes.end(); before_itr++)
+            {
+                for(auto after_itr = after_nodes.begin(); after_itr != after_nodes.end(); after_itr++)
+                {
+                    double v0 = before_itr->vel.linear_velocity;
+                    double v1 = after_itr->vel.linear_velocity;
+                    double a = (v1*v1-v0*v0)/(2*l);
+                    if(a > minimum_accerelation_ && a < maximum_accerelation_)
+                    {
+                        connection_finded = true;
+                        Edge edge;
+                        edge.before_node = *before_itr;
+                        edge.after_node = *after_itr;
+                        edge.linear_accerelation = a;
+                        edges.push_back(edge);
+                    }
+                }
+            }
+            if(!connection_finded)
+            {
+                return boost::none;
+            }
+        }
+        return edges;
+    }
+
+    std::vector<Node> VelocityGraph::makeNodes(hermite_path_msgs::msg::ReferenceVelocity vel)
+    {
+        std::vector<Node> ret;
+        double v = 0.0;
+        int count = 0;
+        while(v>vel.linear_velocity)
+        {
+            v = velocity_resoluation_*count;
+            Node n;
+            n.vel.t = vel.t;
+            n.vel.linear_velocity = vel.linear_velocity;
+            ret.push_back(n);
+            count++;
+        }
+        return ret;
+    }
+}
