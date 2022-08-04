c9672df59b80d0d40419899449f06d3f1700996c
asa-naki <aki.grade2580@outlook.jp>
2022-08-04 20:08:11 +0900

    add costmap_planner component and node

---
+++ velocity_planner/include/velocity_planner/costmap_planner_component.hpp
@@ -0,0 +1,75 @@
+// Copyright (c) 2022 OUXT Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VELOCITY_PLANNER__COSTMAP_PLANNER_COMPONENT_HPP_
+#define VELOCITY_PLANNER__COSTMAP_PLANNER_COMPONENT_HPP_
+
+#if __cplusplus
+extern "C" {
+#endif
+
+// The below macros are taken from https://gcc.gnu.org/wiki/Visibility and from
+// demos/composition/include/composition/visibility_control.h at https://github.com/ros2/demos
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef __GNUC__
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_EXPORT __attribute__((dllexport))
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_IMPORT __attribute__((dllimport))
+#else
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_EXPORT __declspec(dllexport)
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_IMPORT __declspec(dllimport)
+#endif
+#ifdef VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_BUILDING_DLL
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC \
+  VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_EXPORT
+#else
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC \
+  VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_IMPORT
+#endif
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC_TYPE \
+  VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_LOCAL
+#else
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_EXPORT __attribute__((visibility("default")))
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_IMPORT
+#if __GNUC__ >= 4
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC __attribute__((visibility("default")))
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_LOCAL __attribute__((visibility("hidden")))
+#else
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_LOCAL
+#endif
+#define VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC_TYPE
+#endif
+
+#if __cplusplus
+}  // extern "C"
+#endif
+
+#include <boost/algorithm/clamp.hpp>
+#include <boost/optional.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace velocity_planner
+{
+class CostmapPlannerComponent : public rclcpp::Node
+{
+public:
+  VELOCITY_PLANNER_COSTMAP_PLANNER_COMPONENT_PUBLIC
+  explicit CostmapPlannerComponent(const rclcpp::NodeOptions & options);
+
+private:
+};
+}  // namespace velocity_planner
+
+#endif  // VELOCITY_PLANNER__COSTMAP_PLANNER_COMPONENT_HPP_
