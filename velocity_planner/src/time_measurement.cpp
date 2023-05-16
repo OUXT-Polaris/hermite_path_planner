3a915aef26202d7a1c560567d0c095c85d0f3bc7
Masaya Kataoka <ms.kataoka@gmail.com>
2023-05-16 23:15:00 +0900

    add velocity graph
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- velocity_planner/src/time_measurement.cpp
+++ velocity_planner/src/time_measurement.cpp
@@ -1,32 +1,49 @@
+#include <chrono>
+#include <hermite_path_planner/hermite_path_generator.hpp>
 #include <iostream>
 #include <vector>
-#include <chrono>
+#include <velocity_planner/velocity_graph.hpp>
 #include <velocity_planner/velocity_planning.hpp>
+
 int main()
 {
   std::vector<hermite_path_msgs::msg::ReferenceVelocity> constraints(100);
   double acceleration_limit = 10;
   double deceleration_limit = -10;
   double velocity_limit = 2.0;
-  std::vector<hermite_path_msgs::msg::ReferenceVelocity> astar_converted_constraints;
-  std::vector<hermite_path_msgs::msg::ReferenceVelocity> new_converted_constraints;
   for (int i = 0; i < 100; i++) {
     constraints[i].t = i;
     constraints[i].linear_velocity = 1;
     constraints[i].stop_flag = false;
   }
   constraints[99].stop_flag = true;
-  astar_converted_constraints = velocity_planning::planVelocity(
-    constraints, acceleration_limit, deceleration_limit, velocity_limit);
-
-  std::chrono::system_clock::time_point start, end;  // 型は auto で可
-  start = std::chrono::system_clock::now();          // 計測開始時間
-  velocity_planning::planVelocity(
-    constraints, acceleration_limit, deceleration_limit, velocity_limit);
-  
-  end = std::chrono::system_clock::now();  // 計測終了時間
-  double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start)
-                     .count();  //処理に要した時間をミリ秒に変換
-  std::cout << "elapsed:" << elapsed << "us" << std::endl;
+  {
+    geometry_msgs::msg::Pose start_pose, goal_pose;
+    goal_pose.position.x = 99.0;
+    std::chrono::system_clock::time_point start, end;
+    hermite_path_planner::HermitePathGenerator generator(1);
+    const auto path = generator.generateHermitePath(start_pose, goal_pose);
+    hermite_path_msgs::msg::HermitePathStamped path_stamped;
+    path_stamped.path = path;
+    path_stamped.reference_velocity = constraints;
+    start = std::chrono::system_clock::now();
+    velocity_planner::VelocityGraph graph(
+      path_stamped, 0.1, acceleration_limit, deceleration_limit, velocity_limit);
+    graph.getPlan();
+    end = std::chrono::system_clock::now();
+    std::cout << "A* method, elapsed:"
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us"
+              << std::endl;
+  }
+  {
+    std::chrono::system_clock::time_point start, end;
+    start = std::chrono::system_clock::now();
+    velocity_planning::planVelocity(
+      constraints, acceleration_limit, deceleration_limit, velocity_limit);
+    end = std::chrono::system_clock::now();
+    std::cout << "Non A* method, elapsed:"
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us"
+              << std::endl;
+  }
   return 0;
 }
